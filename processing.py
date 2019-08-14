c15c52d73c0f4813843a86236030857d0f4f5b15
nathanjin2016 <49109108+nathanjin2016@users.noreply.github.com>
2019-08-14 12:09:46 -0500

    Delete processing.py

--- processing.py
+++
@@ -1,8 +0,0 @@
-import statistics
-
-def calculate_sd(number_list):
-    try:
-        return "The Standard Deviation of the input data is: {}".format(statistics.stdev(number_list))
-    except statistics.StatisticsError as exc:
-        return "Error calculating StDev: {}".format(exc)
-
